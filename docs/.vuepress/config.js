c1c1eabc882fefb25f8564a1815287423d9a872b
nal <nakata.kzhr@gmail.com>
2021-09-20 22:36:08 +0900

    docs: サイドバーの追加

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,4 +3,31 @@ module.exports = {
     description: "Vue.js-jp Vue3 Handson",
     dest: "dist/",
     base: '/handson-vue3/',
+    themeConfig: {
+        sidebar: [
+            '/',
+            {
+                title: '準備編',
+                collapsable: false,
+                children: [
+                    '/setup'
+                ],
+                initialOpenGroupIndex: 1
+            },
+            {
+                title: '本編',
+                collapsable: false,
+                children: [
+                    '/create',
+                    '/overview',
+                    '/rendering',
+                    '/v-for',
+                    '/v-if',
+                    '/methods',
+                    '/event'
+                ],
+                initialOpenGroupIndex: 1
+            }
+        ]
+    }
 };
