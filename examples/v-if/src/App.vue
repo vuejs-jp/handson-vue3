03850a75bbee72554af0d4d1fbbc5e59a45bf46f
Kazuyuki Miyake <k-miyake@zengeeks.io>
2022-02-15 16:20:32 +0900

    v-if のサンプルをコード参照に変更

--- examples/v-if/src/App.vue
+++ examples/v-if/src/App.vue
@@ -1,3 +1,4 @@
+// region template
 <template>
   <header class="header">
     <img
@@ -26,7 +27,9 @@
     </template>
   </main>
 </template>
+// endregion template
 
+// region script
 <script>
 export default {
   name: 'App',
@@ -74,6 +77,7 @@ export default {
   }
 }
 </script>
+// endregion script
 
 <style>
 body {
