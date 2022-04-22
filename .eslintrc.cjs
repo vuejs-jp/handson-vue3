916f6d1190d8818d4e6d111a512923ebbf18ee58
nal <nakata.kzhr@gmail.com>
2022-04-22 10:33:46 +0900

    chore: ビルドツールをVue CLI -> Viteに変更 (#126)
    * chore: プロジェクト作成をVue CLI -> create-vueに変更
    
    * chore: ESLint設定をcreate-vueに合わせる
    
    * feature: App.vueをscript setupで書き直す
    
    * chore: VSCodeでの保存時にESLintを実行する
    
    * chore: core-jsを削除
    
    * chore: .eslintrc.js -> .eslintrc.cjs に変更
    
    * chore: babelは不要になったので設定ファイルを消す

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    'vue/setup-compiler-macros': true
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier'
+  ],
+  rules: {
+    'vue/html-closing-bracket-newline': [
+      'error',
+      {
+        singleline: 'never',
+        multiline: 'never'
+      }
+    ],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': 'off' // eslint-plugin-prettierの部分だけoffにする
+  }
+}
