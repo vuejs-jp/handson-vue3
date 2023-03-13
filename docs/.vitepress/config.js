ef5399aced79aba2ab9f3b4ac7bf218e263b69a8
Kazuyuki Miyake <k-miyake@zengeeks.io>
2023-03-13 21:03:06 +0900

    サイドバーを定義

--- docs/.vitepress/config.js
+++
@@ -1,5 +0,0 @@
-export default {
-  title: 'Vue3 Hands-on',
-  description: 'Vue.js-jp Vue3 Hands-on',
-  head: [['link', { rel: 'icon', href: '/favicon-32.png' }]]
-}
---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,62 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  lang: 'ja-JP',
+  title: 'Vue3 Hands-on',
+  description: 'Vue.js-jp Vue3 Hands-on',
+
+  // TODO: favicon
+  // head: [['link', { rel: 'icon', href: '/favicon-32.png' }]],
+
+  themeConfig: {
+    siteTitle: 'Vue3 ハンズオン',
+    sidebar: [
+      {
+        text: '準備編',
+        items: [
+          {
+            text: '環境構築',
+            link: '/setup'
+          },
+          {
+            text: 'プロジェクトの作成',
+            link: '/create'
+          }
+        ]
+      },
+      {
+        text: '本編',
+        items: [
+          {
+            text: 'ハンズオンの概要',
+            link: '/overview'
+          },
+          {
+            text: 'data を定義し、商品をレンダリングする',
+            link: '/rendering'
+          },
+          {
+            text: 'v-for で商品を複数表示する',
+            link: '/v-for'
+          },
+          {
+            text: 'v-if で表示・非表示を切り替える',
+            link: '/v-if'
+          },
+          {
+            text: '関数で価格にカンマを入れる',
+            link: '/methods'
+          },
+          {
+            text: '@click で商品を選択する',
+            link: '/event'
+          },
+          {
+            text: '商品をコンポーネント化する',
+            link: '/component'
+          }
+        ]
+      }
+    ]
+  }
+})
