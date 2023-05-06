f051a3e0cead8c523726ee7fa31f94ec29107d83
totocalcio <totohyper@yahoo.co.jp>
2023-05-06 16:42:29 +0900

    exampleにslotを追加

---
+++ examples/slot/src/App.vue
@@ -0,0 +1,142 @@
+// region template
+<template>
+  <header class="header">
+    <img
+      src="/images/logo.svg"
+      alt="">
+    <h1>Vue.js ハンズオン</h1>
+  </header>
+  <main class="main">
+    <template
+      v-for="item in items"
+      :key="item.id">
+      <div
+        v-if="!item.soldOut"
+        class="item"
+        :class="{ 'selected-item': item.selected }"
+        @click="item.selected = !item.selected">
+        <Card
+          :id="item.id"
+          :image="item.image"
+          :name="item.name"
+          :description="item.description"
+          :price="item.price">
+          <template #body>{{ item.description }}</template>
+        </Card>
+      </div>
+    </template>
+  </main>
+</template>
+// endregion template
+
+// region script
+<script setup>
+import { ref } from 'vue'
+import Card from './components/Card.vue'
+
+const items = ref([
+  {
+    id: 1,
+    name: 'アボカドディップバケット',
+    description:
+      '刻んだ野菜をアボカドと混ぜてディップに。こんがり焼いたバゲットとお召し上がりください。',
+    price: 480,
+    image: '/images/item1.jpg',
+    soldOut: false,
+    selected: false
+  },
+  {
+    id: 2,
+    name: 'あの日夢見たホットケーキ',
+    description:
+      '子供のころに食べたかった、あのホットケーキを再現しました。素朴でどこか懐かしい味をどうぞ。',
+    price: 1180,
+    image: '/images/item2.jpg',
+    soldOut: false,
+    selected: false
+  },
+  {
+    id: 3,
+    name: 'HOP WTR',
+    description:
+      'ロサンゼルス生まれのスパークリングウォーター。ノンカロリー、ノンアルコールの新感覚飲料です。',
+    price: 320,
+    image: '/images/item3.jpg',
+    soldOut: true,
+    selected: false
+  },
+  {
+    id: 4,
+    name: 'チーズフレンチフライ',
+    description:
+      'イタリア産チーズをたっぷりかけたアツアツのフレンチフライ。みんな大好きな一品です。',
+    price: 670,
+    image: '/images/item4.jpg',
+    soldOut: false,
+    selected: false
+  }
+])
+
+</script>
+// endregion script
+
+// region style
+<style>
+body {
+  font-family: sans-serif;
+  margin: 0;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+#app {
+  width: 90%;
+  margin: 0 5%;
+  text-align: center;
+  color: #242424;
+}
+
+.header {
+  display: flex;
+  align-content: center;
+  align-items: center;
+  margin-top: 40px;
+  margin-bottom: 40px;
+}
+
+.header > img {
+  width: 100px;
+  height: 100px;
+  margin-right: 20px;
+}
+
+.header > h1 {
+  font-size: 80px;
+  font-weight: bold;
+  line-height: 80px;
+  margin-top: 0;
+  margin-bottom: 0;
+}
+
+.main {
+  display: grid;
+  grid-template-columns: 3fr 3fr 3fr 3fr;
+  column-gap: 24px;
+  row-gap: 24px;
+}
+
+.item {
+  padding: 10px;
+  cursor: pointer;
+}
+
+.item:hover {
+  transition: 0.2s transform ease-out;
+  transform: scale(1.05);
+}
+
+.selected-item {
+  background: #e3f2fd;
+}
+</style>
+// endregion style
---
+++ examples/slot/src/components/Card.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="thumbnail">
+    <img
+      :src="image"
+      alt="">
+  </div>
+  <div class="description">
+    <h2>{{ name }}</h2>
+    <p><slot name="body" /></p>
+    <span>¥<span class="price">{{ pricePrefix(price) }}</span></span>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    default: '',
+    required: false
+  },
+  price: {
+    type: Number,
+    default: null,
+    required: false
+  },
+  image: {
+    type: String,
+    default: '',
+    required: false
+  },
+});
+
+/**
+ * 価格を3桁ごとのカンマ付きで返す
+ * @param {number} price 価格
+ */
+function pricePrefix(price) {
+  return price.toLocaleString()
+}
+</script>
+
+<style>
+.item > div.thumbnail > img {
+  width: 100%;
+  height: calc(100%);
+  object-fit: cover;
+}
+
+.item > div.description {
+  text-align: left;
+  margin-top: 20px;
+}
+
+.item > div.description > p {
+  margin-top: 0px;
+  margin-bottom: 0px;
+  font-size: 18px;
+  line-height: 25px;
+}
+
+.item > div.description > span {
+  display: block;
+  margin-top: 10px;
+  font-size: 20px;
+}
+
+.item > div.description > span > .price {
+  font-size: 28px;
+  font-weight: bold;
+}
+</style>
