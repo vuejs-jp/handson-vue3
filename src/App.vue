b874ab783421367168e4f2455c58ae08b13504e1
Kazuyuki Miyake <k-miyake@zengeeks.io>
2023-06-08 21:20:53 +0900

    7/12 ハンズオンイベント用のブランチを作成

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { ref } from 'vue'
-import Card from './components/Card.vue'
 
 const items = ref([
   {
@@ -45,6 +44,13 @@ const items = ref([
   }
 ])
 
+/**
+ * 価格を3桁ごとのカンマ付きで返す
+ * @param {number} price 価格
+ */
+function pricePrefix(price) {
+  return price.toLocaleString()
+}
 </script>
 
 <template>
@@ -63,12 +69,16 @@ const items = ref([
         class="item"
         :class="{ 'selected-item': item.selected }"
         @click="item.selected = !item.selected">
-        <Card
-          :id="item.id"
-          :image="item.image"
-          :name="item.name"
-          :description="item.description"
-          :price="item.price"/>
+        <div class="thumbnail">
+          <img
+            :src="item.image"
+            alt="">
+        </div>
+        <div class="description">
+          <h2>{{ item.name }}</h2>
+          <p>{{ item.description }}</p>
+          <span>¥<span class="price">{{ pricePrefix(item.price) }}</span></span>
+        </div>
       </div>
     </template>
   </main>
@@ -85,7 +95,6 @@ body {
 #app {
   width: 90%;
   margin: 0 5%;
-  text-align: center;
   color: #242424;
 }
 
@@ -126,6 +135,35 @@ body {
 .item:hover {
   transition: 0.2s transform ease-out;
   transform: scale(1.05);
+}
+
+.item > div.thumbnail > img {
+  width: 100%;
+  height: calc(100%);
+  object-fit: cover;
+}
+
+.item > div.description {
+  text-align: left;
+  margin-top: 20px;
+}
+
+.item > div.description > p {
+  margin-top: 0px;
+  margin-bottom: 0px;
+  font-size: 18px;
+  line-height: 25px;
+}
+
+.item > div.description > span {
+  display: block;
+  margin-top: 10px;
+  font-size: 20px;
+}
+
+.item > div.description > span > .price {
+  font-size: 28px;
+  font-weight: bold;
 }
 
 .selected-item {
